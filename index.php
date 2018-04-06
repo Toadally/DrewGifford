44eb6577f8e0d77a90ea39aa5841e8329d5010c0
ToadallyTerrific <legos30000@hotmail.com>
2018-04-06 09:34:34 -0400

    Added CDN

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<h1>Hello World!</h1>
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700|Roboto:300,400" rel="stylesheet">
+    <link href="/css/index.css" rel="stylesheet">
+    <title>Drew Gifford | Web Design &amp; Development</title>
+
+  </head>
+
+  <body>
+
+    <div class="section full" id="main" data-parallax="scroll" data-image-src="/img/mountains.jpg">
+      <div class="">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+
+    <div class="section">
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+      <h1>Text</h1>
+    </div>
+
+    <!-- SCRIPTS -->
+    <script src="/js/jquery.js"></script>
+    <script src="/js/parallax.min.js"></script>
+  </body>
+
+
+</html>
